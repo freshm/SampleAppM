e45d276f7c38801b15404264128f33917d5d38db
freshm <m.j.ahlgrimm@gmx.de>
2012-12-19 22:15:32 +0100

    Sessions and Edit added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,23 +64,7 @@ describe User do
       it { should_not be_valid }
     end
     
-    describe "when password is not present" do
-      before { @user.password = @user.password_confirmation = "" }
-      it { should_not be_valid }
-    end
-    
-    describe "when password is not present" do
-      before { @user.password = @user.password_confirmation = " " }
-      it { should_not be_valid }
-    end
-  
-  
-    describe "when password doesn't match confirmation" do
-      before { @user.password_confirmation = "mismatch" }
-      it { should_not be_valid }
-    end
-    
-    describe "when password confirmation is nil" do
+    describe "when password is  en password confirmation is nil" do
       before { @user.password_confirmation = nil }
       it { should_not be_valid }
     end
